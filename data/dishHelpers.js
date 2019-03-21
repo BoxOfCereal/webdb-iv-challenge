e9b74c47b71e0f3c9f6bcce3e036764d23f22858
Noah <noah.stoeckel@gmail.com>
2019-03-21 19:57:04 -0400

    Add addDish Helper

--- data/dishHelpers.js
+++ data/dishHelpers.js
@@ -5,5 +5,10 @@ const db = knex(dbConfig.development);
 module.exports = {
 	getDishes: () => {
 		return db("dishes");
+	},
+	addDish: dish => {
+		return db("dishes")
+			.insert(dish)
+			.then(ids => ids[0]);
 	}
 };
--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const db = require("./data/dishHelpers.js");
 db.getDishes().then(dishes => {
 	console.log(dishes);
 });
+
+db.addDish({ dish_name: "cake" }).then(dishes => {
+	console.log(dishes);
+});
